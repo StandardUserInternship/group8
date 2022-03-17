abbfe9eb971c31008f52339f2c2fd2ec0c9754b0
KaiWilk98 <kwilkinson@standarduserllc.com>
2022-03-17 10:52:41 -0500

    Co-authored-by: jesusrojas123 <jesusrojas123@users.noreply.github.com>

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -10,10 +11,15 @@ def index():
 def admin():
     return render_template('admin.html', title='Admin')
 
-@app.route('/login')
-def admin():
-    return render_template('login.html', title='Login')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Login', form=form)
 
 @app.route('/user')
-def admin():
+def userProfile():
     return render_template('userProfile.html', title='Profile')
